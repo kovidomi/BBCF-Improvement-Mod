63f35297380ed7bf99714619daa398cc6eb1f63f
kovidomi <kovidomi01@citromail.hu>
2020-06-07 17:44:30 +0200

    Update info.h

--- src/Core/info.h
+++ src/Core/info.h
@@ -8,8 +8,3 @@
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
 #define MOD_LINK_FORUM L"https://steamcommunity.com/app/586140/discussions/0/1291817208497395528/"
 #define MOD_LINK_GITHUB L"https://github.com/kovidomi/BBCF-Improvement-Mod"
-
-#define MOD_LINK_DONATE L"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C8EDR78DJ8PU2"
-#define MOD_LINK_DONATORS L"https://github.com/kovidomi/BBCF-Improvement-Mod/raw/master/resource/donators.txt"
-#define MOD_LINK_PALETTES_ARCHIVE L"https://github.com/kovidomi/BBCF-Improvement-Mod/raw/master/resource/palettes.tar.gz"
-#define MOD_LINK_PALETTES_HASH L"https://github.com/kovidomi/BBCF-Improvement-Mod/raw/master/resource/palettes_hash.txt"
