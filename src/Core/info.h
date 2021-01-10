84ad0d2d3951fcb8f39ee617f20eb8ccbe703e23
kovidomi <kovidomi01@citromail.hu>
2021-01-10 23:46:33 +0100

    Change version number

--- src/Core/info.h
+++ src/Core/info.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define MOD_VERSION_NUM	"v2.99 (PRE-RELEASE)"
+#define MOD_VERSION_NUM	"v3.00"
 #define MOD_WINDOW_TITLE "BBCF Improvement Mod"
 
-#define MOD_FORCE_DISABLE_UPDATE_CHECK 1
+#define MOD_FORCE_DISABLE_UPDATE_CHECK 0
 
 // Links
 #define MOD_LINK_DISCORD L"https://discord.gg/j2mCX9s"
